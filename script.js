0118edf01d36638791dca61de0a2afd47f5455f9
thisispete16 <petelastcallhockey@gmail.com>
2026-01-07 18:49:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // List of JPEG images
 const images = [
-  "images/dailies(1).jpg",
-  "images/dailies(2).jpg",
-  "images/dailies(3).jpg",
-  "images/dailies(4).jpg"
+  "images/dailies1.jpg",
+  "images/dailies2.jpg",
+  "images/dailies3.jpg",
+  "images/dailies4.jpg"
 ];
 
 // Pick a random image
-//const randomIndex = Math.floor(Math.random() * images.length);
+const randomIndex = Math.floor(Math.random() * images.length);
 const selectedImage = "images/dailies (1).jpg";
 
 // Set image source
-//const imgElement = document.getElementById("randomImage");
-//imgElement.src = selectedImage;
+const imgElement = document.getElementById("randomImage");
+imgElement.src = selectedImage;
