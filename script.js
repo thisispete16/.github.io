869d2d6efaf2b14686a6fd00a55aac09035e0cd3
thisispete16 <petelastcallhockey@gmail.com>
2026-01-07 20:54:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // List of JPEG images
 const images = [
-  "images/dailies1.jpg",
-  "/images/dailies1.jpg",
-  "/images/dailies2.jpg",
-  "/images/dailies3.jpg",
-  "/images/dailies4.jpg"
+  "./images/dailies1.jpg",
+  "./images/dailies1.jpg",
+  "./images/dailies2.jpg",
+  "./images/dailies3.jpg",
+  "./images/dailies4.jpg"
 ];
 
 // Pick a random image
